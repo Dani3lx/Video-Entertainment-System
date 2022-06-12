6e0445169c89662ad375fc82e4041195295b932a
makathe2 <katee.ma@mail.utoronto.ca>
2022-06-12 19:47:12 -0400

    fixed createUser file

--- phase0/src/CreateUser.java
+++ phase0/src/CreateUser.java
@@ -1,25 +1,39 @@
 
 public class CreateUser {
+    //cna take away constructor, if not necessary
+//    private  String username;
+//    private  String password;
+//    private boolean adminorNon;
+//
+//    public CreateUser(String user, String pass, boolean AdminNonAdmin)
+//    {
+//        username = user;
+//        password=pass;
+//        adminorNon = AdminNonAdmin;
+//    }
+//
+//    public String getUsername(){
+//        return username;
+//    }
+//
+//    public String getPassword(){
+//        return password;
+//    }
+//
+//    public boolean getadminorNon(){
+//        return adminorNon;
+//    }
 
-    private  String username;
-    private  String password;
-
-    public CreateUser(String user, String pass)
-    {
-        username = user;
-        password=pass;
-    }
-
-    public String getUsername(){
-        return username;
-    }
-
-    public String getPassword(){
-        return password;
-    }
-
-    public void create(String user, String pass){
-        //createUser person = new createUser(user, pass);
-        UserManager.createUser(username, password);
+    public boolean create(String user, String pass, boolean adminOrNot){
+        //Calls appropriate method in UserManager to create account
+        //Returns whether creation was successful or not
+        boolean success = false;
+        if (adminOrNot){
+            success = UserManager.createAdminUser(user, pass);          //need fugure out if these createUser methods are isntance or static
+        }
+        else{
+            success = UserManager.createNonAdminUser(user, pass);
+        }
+        return success;
     }
 }
