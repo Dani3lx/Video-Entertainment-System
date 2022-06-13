634d0adde0c04986afbf0c18678c592b638b5aef
makathe2 <katee.ma@mail.utoronto.ca>
2022-06-12 21:23:38 -0400

    creating basic methods inside Main

--- phase0/src/Main.java
+++ phase0/src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
+    private Scanner sc= new Scanner(System.in);
+    public static void displayBeforelogin(){
+
+    }
+    public void displayBeforeCreateAccount(){
+
+    }
+    public void displayEnterAccount(String username, String password){
+        UserManager usermanager = new UserManager();
+        if(usermanager.validateUser(username, password)){
+
+        }
+    }
+    public void main(String[] args) {
 
     }
 }
