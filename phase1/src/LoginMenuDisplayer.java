c1f5e728e27ca286654d7233db521193faae8596
Dani3lx <1danielxu@gmail.com>
2022-07-06 15:26:49 -0400

    Relocated the inputs and outputs from the LoginMenuDisplayer to the Presenter class

--- phase1/src/LoginMenuDisplayer.java
+++ phase1/src/LoginMenuDisplayer.java
@@ -6,13 +6,12 @@ import java.util.Objects;
 import java.util.Scanner;
 
 public class LoginMenuDisplayer {
-//    UserManager um = new UserManager();
     private UserManager um;
     UserInterfaceHandler UIhandler;
     Presenter p = new Presenter();
     DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
-    public LoginMenuDisplayer(UserManager um){
+    public LoginMenuDisplayer(UserManager um) {
         this.um = um;
         UIhandler = new UserInterfaceHandler(um);
     }
@@ -24,55 +23,46 @@ public class LoginMenuDisplayer {
      */
     public void startMenu() throws IOException {
         // test
-        Scanner sc = new Scanner(System.in);
-        System.out.println(p.startMenuOption("Type 1 to login, type 2 to create a new user account, type 3" +
-                " to exit program"));
+        int userChoice = p.startMenuOptions();
 
         User currentUser;
-        if (sc.hasNextInt()) {
-            int input = (sc.nextInt());
 
-            switch (input) {
-                case 1:
-                    currentUser = UIhandler.loginUser();
-                    if (Objects.isNull(currentUser)) {
-                        System.out.println("Failed to Login");
-                        this.startMenu();
+        switch (userChoice) {
+            case 1:
+                currentUser = UIhandler.loginUser();
+                if (Objects.isNull(currentUser)) {
+                    p.displayErrorMessage("login");
+                    this.startMenu();
+                } else {
+                    um.updateHistory(currentUser, LocalDateTime.now().format(formatter));
+                    if (currentUser.isAdminInd()) {
+                        p.displayAlertMessage("adminLogin");
+                        AfterLoginMenu(currentUser);
                     } else {
-
-                        um.updateHistory(currentUser, LocalDateTime.now().format(formatter));
-                        if (currentUser.isAdminInd()) {
-                            System.out.println(p.alertText("you are now logged in to an admin account"));
-                            AfterLoginMenu(currentUser);
-                        } else {
-                            System.out.println(p.alertText("you are now logged in to a non-admin account"));
-                            AfterLoginMenu(currentUser);
-                        }
-                    }
-                case 2:
-                    currentUser = UIhandler.createUser();
-                    if (Objects.isNull(currentUser)) {
-                        System.out.println("Failed to create a new account");
-                        this.startMenu();
-                    } else {
-                        um.updateHistory(currentUser, LocalDateTime.now().format(formatter));
-                        System.out.println("New account has been created");
+                        p.displayAlertMessage("nonAdminLogin");
                         AfterLoginMenu(currentUser);
                     }
-                case 3:
-                    DataManager sm = new DataManager(um);
-                    sm.setUsers((ArrayList<User>) um.getAllUsers());
-                    sm.saveData("phase1/Data.csv");
-                    System.exit(0);
-                default:
-                    System.out.println("Please enter a valid response");
-                    startMenu();
-            }
-
-        } else {
-            System.out.println("Please enter a valid response");
-            startMenu();
+                }
+            case 2:
+                currentUser = UIhandler.createUser();
+                if (Objects.isNull(currentUser)) {
+                    p.displayErrorMessage("accountCreation");
+                    this.startMenu();
+                } else {
+                    um.updateHistory(currentUser, LocalDateTime.now().format(formatter));
+                    p.displayAlertMessage("accountCreation");
+                    AfterLoginMenu(currentUser);
+                }
+            case 3:
+                DataManager sm = new DataManager(um);
+                sm.setUsers((ArrayList<User>) um.getAllUsers());
+                sm.saveData("phase1/Data.csv");
+                System.exit(0);
+            default:
+                p.displayErrorMessage("userInput");
+                startMenu();
         }
+
     }
 
     /**
@@ -81,31 +71,16 @@ public class LoginMenuDisplayer {
 
     private void AfterLoginMenu(User user) throws IOException {
 
-        if (!(user.isAdminInd())) {
-            System.out.println("Please input one of the following number to proceed " +
-                    "\n 1 - Change Password \n 2 - Check login history \n 3 - Log out \n\n\n");
-        } else {
-            System.out.println("Please input one of the following number to proceed " +
-                    "\n 1 - Change Password \n 2 - Check login history \n 3 - Log out \n 4 - Create AdminUser \n" +
-                    " 5 - Delete User \n 6 - Ban User \n 7 - UnBan User \n");
-        }
-
-        Scanner sc = new Scanner(System.in);
-
-        if (sc.hasNextInt()) {
-            int result = sc.nextInt();
-            sc.nextLine();
+        int result = p.afterLoginOptions(user.isAdminInd());
             switch (result) {
                 case 1:
-                    System.out.println("Please enter a new password");
-                    String newPassword = sc.nextLine();
-                    um.changePassword(user, newPassword);
-                    System.out.println("Password change was successful\n");
+                    um.changePassword(user, p.getPassword());
+                    p.displayAlertMessage("passwordChange");
                     break;
                 case 2:
-                    System.out.println("Checking history:");
+                    p.displayOptionMessages("checkHistory");
                     um.checkHistory(user);
-                    System.out.println("\n");
+                    p.displayOptionMessages("lineBreak");
                     break;
                 case 3:
                     startMenu();
@@ -134,12 +109,8 @@ public class LoginMenuDisplayer {
                         UIhandler.unBanUser();
                     }
                     break;
-                default:
-                    System.out.println("Please enter a valid input");
             }
-        } else {
-            System.out.println("Please enter a valid input");
-        }
+
         AfterLoginMenu(user);
     }
 }
--- phase1/src/Presenter.java
+++ phase1/src/Presenter.java
@@ -1,15 +1,95 @@
+import java.util.Scanner;
+
 public class Presenter {
-    public String startMenuOption(String input) {
+
+    private String startMenuOption(String input) {
         int num = input.length();
         StringBuilder decorator = new StringBuilder();
         decorator.append("*".repeat(num));
         return decorator + "\n" + input + "\n" + decorator;
     }
 
-    public String alertText(String input) {
+    private String alertText(String input) {
         int num = input.length();
         StringBuilder decorator = new StringBuilder();
         decorator.append("-".repeat(num));
         return decorator + "\n" + input + "\n" + decorator;
+    }
+
+    public int startMenuOptions() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(startMenuOption("Type 1 to login, type 2 to create a new user account, type 3" +
+                " to exit program"));
+        if (sc.hasNextInt()) {
+            return (sc.nextInt());
+        } else {
+            return 0;
+        }
+    }
+
+    public int afterLoginOptions(boolean isAdmin){
+        if (isAdmin) {
+            System.out.println("Please input one of the following number to proceed " +
+                    "\n 1 - Change Password \n 2 - Check login history \n 3 - Log out \n 4 - Create AdminUser \n" +
+                    " 5 - Delete User \n 6 - Ban User \n 7 - UnBan User \n");
+        } else {
+            System.out.println("Please input one of the following number to proceed " +
+                    "\n 1 - Change Password \n 2 - Check login history \n 3 - Log out \n\n\n");
+        }
+
+        Scanner sc = new Scanner(System.in);
+
+        if (sc.hasNextInt()) {
+            return sc.nextInt();
+        } else {
+            return 0;
+        }
+    }
+
+    public String getPassword() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter a new password");
+        return sc.nextLine();
+    }
+
+    public void displayErrorMessage(String error) {
+        switch (error) {
+            case "login":
+                System.out.println("Login was unsuccessful");
+                break;
+            case "accountCreation" :
+                System.out.println("Account creation was unsuccessful");
+                break;
+            case "userInput":
+                System.out.println("Please enter a valid input");
+                break;
+        }
+    }
+
+    public void displayAlertMessage(String alert) {
+        switch (alert) {
+            case "adminLogin" :
+                System.out.println(alertText("you are now logged in to an admin account"));
+                break;
+            case "nonAdminLogin" :
+                System.out.println(alertText("you are now logged in to an non-admin account"));
+                break;
+            case "accountCreation":
+                System.out.println(alertText("A new account has been successfully created"));
+                break;
+            case "passwordChange":
+                System.out.println("Password change was successful\n");
+                break;
+        }
+    }
+
+    public void displayOptionMessages(String option) {
+        switch (option) {
+            case "checkHistory":
+                System.out.println("Checking history:");
+                break;
+            case "lineBreak":
+                System.out.println("\n");
+        }
     }
 }
