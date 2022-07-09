081095f44f1c3108079eba08bbad3570d30dbb02
akmar <akmar.chowdhury@mail.utoronto.ca>
2022-07-08 21:03:37 -0400

    Updated Video with getters/setters

--- phase1/src/Video.java
+++ phase1/src/Video.java
@@ -2,6 +2,7 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 public class Video {
+
     private String content;
     private String uniqueID;
     private String description;
@@ -13,8 +14,63 @@ public class Video {
     // private ratings
 
     public Video(){
+        //need to decide how we initialize the video construct
+        //it will heavily depend on upload video in videomanager
 
     }
 
+    // getters - retrieve video information
+
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getUniqueID() {
+        return uniqueID;
+        //Todo check if it is wise to allow this to be public, maybe protected?
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUploader() {
+        return uploader;
+    }
+
+    public LocalDateTime getDate_upload() {
+        return date_upload;
+    }
+
+    public ArrayList<String> getHistory() {
+        return history;
+    }
+
+    public ArrayList<String> getCategories() {
+        return categories;
+    }
+
+    // Setters - We will allow users/programs change these data fields
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    //todo include append and remove categories as well since setCategories only adds a predetermined arraylist to replace the current one
+    public void setCategories(ArrayList<String> categories) {
+        this.categories = categories;
+    }
 }
 
