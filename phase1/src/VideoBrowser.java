fe226f9a025246d7db6883bbbc7b7d2ee93bec5d
Dani3lx <1danielxu@gmail.com>
2022-07-09 16:40:21 -0400

    Wrote down the skeleton for VideoBrowser

---
+++ phase1/src/VideoBrowser.java
@@ -0,0 +1,14 @@
+import java.util.Collections;
+import java.util.Scanner;
+// Will decide which collection later
+
+public class VideoBrowser {
+    public Collections BrowseByName() {
+        Scanner sc = new Scanner(System.in);
+        if (sc.hasNextInt()) {
+            // todo
+        } else {
+            // todo
+        }
+    }
+}
