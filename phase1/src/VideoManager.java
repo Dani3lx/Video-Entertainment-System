39c95360b37d3c3210505ab5931e0984436f1ce7
makathe2 <katee.ma@mail.utoronto.ca>
2022-07-10 00:02:40 -0400

    Added VideoManager class

---
+++ phase1/src/VideoManager.java
@@ -0,0 +1,88 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class VideoManager {
+    private List<Video> vids;
+
+
+
+    public void setAllVids(List<Video> vids) {
+        this.vids = vids;
+    }
+    //Consider storing instance of Video in User
+    //format of video file, url
+    //maybe format so that if uploader already in system, append vidLink to ArrayList<Video>
+    //returns if video upload successful
+    public boolean uploadVideo(String uploader, String title, String description, ArrayList<String> categories,String vidLink){
+        for (Video v:vids){
+            if(vidLink.equalsIgnoreCase(v.getContent())){
+                return false;
+            }
+        }
+
+        Video v1 = new Video(uploader, title, description, categories, vidLink);
+        return true;
+    }
+
+
+    public boolean deleteVideo(String vidLink){
+        for (Video v: vids){
+            if (vidLink.equalsIgnoreCase(v.getContent())){
+
+            }
+        }
+    }
+
+    //need know what user wants to edit
+    //ex.
+    public boolean editVideo(String vidLink){
+        for (Video v: vids){
+            if (vidLink.equalsIgnoreCase(v.getContent())){
+
+            }
+        }
+    }
+
+
+    //return type allows for video with same title
+    public List<Video> retrieveVideobyTitle(String name){
+        ArrayList<Video> vid_list = new ArrayList<>();
+        for (Video v: vids){
+            if (name.equalsIgnoreCase(v.getName())){
+                vid_list.add(v);
+            }
+
+        }
+        return vid_list;
+    }
+
+    public List<Video> retrieveVideobyCategory(String category){
+        ArrayList<Video> vid_list = new ArrayList<>();
+        for (Video v: vids){
+            for (String cate: v.getCategories()){
+                if (category.equalsIgnoreCase(cate)){
+                    vid_list.add(v);
+                }
+            }
+
+        }
+        return vid_list;
+    }
+
+    public List<Video> retrieveVideobyUploader(String uploader){
+        ArrayList<Video> vid_list = new ArrayList<>();
+        for (Video v: vids){
+
+            if (uploader.equalsIgnoreCase(v.getUploader())){
+                vid_list.add(v);
+
+            }
+
+        }
+        return vid_list;
+    }
+
+
+
+
+}
