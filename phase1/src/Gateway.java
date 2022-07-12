90e6672dc41cbe80c3b281f71988a06a116c8312
Dani3lx <1danielxu@gmail.com>
2022-07-12 00:36:04 -0400

    Fixed Gateway class

--- phase1/src/Gateway.java
+++ phase1/src/Gateway.java
@@ -3,17 +3,16 @@ import java.util.ArrayList;
 
 public class Gateway {
     UserManager um = new UserManager();
-    DataManager dm = new DataManager(um);
+    DataManager sm = new DataManager(um);
 
     public void saveChanges(){
-        DataManager sm = new DataManager(um);
         sm.setUsers((ArrayList<User>) um.getAllUsers());
         sm.saveData("phase1/Data.csv");
     }
 
     public void getData() throws IOException {
-        dm.loadData("phase1/Data.csv"); //Read Data from Data.csv
-        um.setAllUsers(dm.getUsers());
+        sm.loadData("phase1/Data.csv"); //Read Data from Data.csv
+        um.setAllUsers(sm.getUsers());
         LoginMenuDisplayer lmd = new LoginMenuDisplayer(um, this);
 
         lmd.startMenu();
