36a54ef34a0e50f32ea35252f5a41fd8766abb50
Dani3lx <1danielxu@gmail.com>
2022-07-23 23:51:46 -0400

    Added documentation to VideoEditor

--- phase1/src/usecase/VideoEditor.java
+++ phase1/src/usecase/VideoEditor.java
@@ -4,44 +4,66 @@ import entities.Video;
 
 import java.util.ArrayList;
 
+/**
+ * This class is responsible for performing all direct interactions with the User entity classes.
+ *
+ * @author Daniel Xu, ...
+ * @version 1.0
+ * @since 2022-07-23
+ */
 public class VideoEditor {
 
     /**
-     * @param v        entities.Video object
-     * @param newTitle The new title user wants their video to be
+     * Edits the video title.
+     *
+     * @param v        target video
+     * @param newTitle the new title user wants their video to be
      */
     public void editTitle(Video v, String newTitle) {
         v.setName(newTitle);
     }
 
     /**
-     * @param v       entities.Video object
-     * @param newCate The new categories user wants their video to be
+     * Edits the categories.
+     *
+     * @param v       target video
+     * @param newCate the new categories that user wants their video to be
      */
     public void editCategories(Video v, ArrayList<String> newCate) {
         v.setCategories(newCate);
     }
 
     /**
-     * @param v      entities.Video object
-     * @param newDes The new description user wants their video to be
+     * @param v      target user
+     * @param newDes the new description user wants their video to be
      */
     public void editDescription(Video v, String newDes) {
         v.setDescription(newDes);
     }
 
+    /**
+     * Likes the video.
+     *
+     * @param v target video
+     */
     public void likeVideo(Video v){
         v.addLikes();
     }
 
+    /**
+     * Dislikes the video.
+     *
+     * @param v the target video
+     */
     public void dislikeVideo(Video v){
         v.addDislikes();
     }
 
-
     /**
-     * @param vid entities.Video object
-     * @return All attributes of the video
+     * Return the video's information.
+     *
+     * @param vid target video
+     * @return video's information
      */
     public String[] returnVideoInformation(Video vid) {
         return new String[]{vid.getName(), vid.getUploader(), vid.getDescription(), vid.getDate_upload(), vid.getContent(), vid.getRatings().get(0), vid.getRatings().get(1)};
