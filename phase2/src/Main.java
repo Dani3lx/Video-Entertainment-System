43fd276bbabf306d03d9e5d3375bcf689ac063a2
Dani3lx <1danielxu@gmail.com>
2022-08-04 18:34:39 -0400

    Created a demo for our new way of storing run time data

--- phase2/src/Main.java
+++ phase2/src/Main.java
@@ -1,9 +1,11 @@
 import gateways.DataManager;
+import presenters.language.EnglishPresenter;
 import usecase.PlaylistManager;
 import usecase.UserManager;
 import usecase.VideoManager;
 import userInterfaces.menu.MenuBuilder;
 import userInterfaces.menu.Menu;
+import userInterfaces.menuAction.MenuActionFactory;
 import userInterfaces.userPrompt.TerminalUserPrompt;
 import userInterfaces.userPrompt.UserPrompt;
 
@@ -23,7 +25,17 @@ public class Main {
 
         UserPrompt userPrompt = new TerminalUserPrompt();
 
+
+
+        // Demo, create a class called RunTimeData that stores all the data that will persist throughout the runtime of the program
         MenuBuilder builder = new MenuBuilder(um, vm, pm, userPrompt, null);
+        
+        MenuActionFactory factory = new MenuActionFactory(um, vm, pm, userPrompt, null);
+        RunTimeData data = new RunTimeData(um, vm, pm, builder, factory);
+        data.setLanguagePresenter(new EnglishPresenter());
+        data.setCurrentUser(null);
+        data.setUserPrompt(new TerminalUserPrompt());
+
         Menu menu = builder.getMenu("start");
         menu.run();
     }
