8873d8293445026c51ae325bd717614bc2bba83e
sfnicholas <ktspsnicholas@gmail.com>
2022-08-05 22:14:25 -0400

    Create variable Hashmap<> userNames; Edit functions to fit it.

--- phase1/src/phase2features/Ratings.java
+++ phase1/src/phase2features/Ratings.java
@@ -1,33 +1,64 @@
 package phase2features;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * THIS IS FOR PHASE 2
  */
 public class Ratings {
-    private ArrayList<String> likeUserName;
-    private Integer totalLikes;
+    private HashMap<String, Integer> userNames; // 0 = dislike, 1 = like
 
-    public Ratings(ArrayList<String> likeUserName){
-        this.totalLikes = likeUserName.size();
-        this.likeUserName = likeUserName;
+    public Ratings(HashMap<String, Integer> userNames){
+        this.userNames = userNames;
+    }
+    public Ratings(){
+        this.userNames = new HashMap<String, Integer>(userNames);
     }
 
     public Integer getTotalLikes() {
-        return totalLikes;
+        int count = 0;
+        for (Integer value: userNames.values()){
+            if (value.equals(1)){
+                count++;
+            }
+        }
+        return count;
     }
+
+    public Integer getTotalDislikes(){
+        int count = 0;
+        for (Integer value: userNames.values()){
+            if (value.equals(0)){
+                count++;
+            }
+        }
+        return count;}
 
     public void addLikes(String username){
-        likeUserName.add(username);
-        totalLikes += 1;
+        userNames.put(username, 1);
     }
     public void addDislikes(String username){
-        likeUserName.add(username);
-        totalLikes += 1;
+        userNames.put(username, 0);
     }
 
-    public ArrayList<String> getLikeUserName() {
-        return likeUserName;
+    public ArrayList<String> getLikeUserNames() {
+        ArrayList<String> likeUserNames = new ArrayList<>();
+        for (String key: userNames.keySet()){
+            if (userNames.get(key).equals(1)){
+                likeUserNames.add(key);
+            }
+        }
+        return likeUserNames;
+    }
+    public ArrayList<String> getDislikeUserNames() {
+        ArrayList<String> DislikeUserNames = new ArrayList<>();
+        for (String key: userNames.keySet()){
+            if (userNames.get(key).equals(0)){
+                DislikeUserNames.add(key);
+            }
+        }
+        return DislikeUserNames;
     }
 }
