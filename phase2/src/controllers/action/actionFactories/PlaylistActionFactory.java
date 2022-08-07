896a8d55f184593e08356274ec39e2539fe0a277
akmar <akmar.chowdhury@mail.utoronto.ca>
2022-08-07 12:39:11 -0400

    Setting up playlist actions

--- phase2/src/controllers/action/actionFactories/PlaylistActionFactory.java
+++ phase2/src/controllers/action/actionFactories/PlaylistActionFactory.java
@@ -1,6 +1,8 @@
 package controllers.action.actionFactories;
 
+import controllers.action.actions.playlistMenu.*;
 import entities.User;
+import controllers.action.actions.videoBrowseMenu.Return;
 
 public class PlaylistActionFactory implements ActionFactory {
     private final User user;
@@ -11,6 +13,14 @@ public class PlaylistActionFactory implements ActionFactory {
 
     public Action getAction(String type) {
         switch (type) {
+            case "search playlist":
+                return new SearchPlaylist(user);
+            case "create playlist":
+                return new CreatePlaylist(user);
+            case "display all playlists":
+                return new DisplayAllPlaylist(user);
+            case "return":
+                return new Return(user);
             default:
                 return null;
         }
