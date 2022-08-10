f40b9cfec20c53ecf8d8bfc9c53c7bace701e76e
Dani3lx <1danielxu@gmail.com>
2022-08-10 15:13:13 -0400

    Documented actionFactories

--- phase2/src/controllers/actionFactories/UserActionFactory.java
+++ phase2/src/controllers/actionFactories/UserActionFactory.java
@@ -6,12 +6,23 @@ import presenters.language.LanguagePresenter;
 import presenters.menuPresenter.MenuPresenter;
 import userInterfaces.userPrompt.UserPrompt;
 
+/**
+ * A concrete action factory that creates user related actions base on type.
+ */
 public class UserActionFactory implements ActionFactory {
     private final User user;
     private final LanguagePresenter lp;
     private final MenuPresenter mp;
     private final UserPrompt userPrompt;
 
+    /**
+     * Creates a user action factory with the given user prompt, user, language presenter and menu presenter.
+     *
+     * @param userPrompt the program's user prompt
+     * @param user       a user
+     * @param lp         the program's language presenter
+     * @param mp         the program's menu presenter
+     */
     public UserActionFactory(UserPrompt userPrompt, User user, LanguagePresenter lp, MenuPresenter mp) {
         this.userPrompt = userPrompt;
         this.user = user;
@@ -19,6 +30,12 @@ public class UserActionFactory implements ActionFactory {
         this.mp = mp;
     }
 
+    /**
+     * Generates and returns an action base on the type.
+     *
+     * @param type the type of action
+     * @return an action with the given type
+     */
     public Action getAction(String type) {
         switch (type) {
             case "logout":
--- phase2/src/controllers/actionFactories/VideoStudioActionFactory.java
+++ phase2/src/controllers/actionFactories/VideoStudioActionFactory.java
@@ -2,26 +2,40 @@ package controllers.actionFactories;
 
 import controllers.actions.videoStudioActions.*;
 import entities.User;
-import entities.Video;
 import presenters.language.LanguagePresenter;
 import presenters.menuPresenter.MenuPresenter;
 import userInterfaces.userPrompt.UserPrompt;
 
+/**
+ * A concrete action factory that creates video studio related actions base on type.
+ */
 public class VideoStudioActionFactory implements ActionFactory {
     private final User user;
     private final LanguagePresenter lp;
     private final MenuPresenter mp;
     private final UserPrompt userPrompt;
-    private Video video;
 
-    public VideoStudioActionFactory(UserPrompt userPrompt, User user, LanguagePresenter lp, MenuPresenter mp, Video video) {
+    /**
+     * Creates a video studio action factory with the given user prompt, user, language presenter and menu presenter.
+     *
+     * @param userPrompt the program's user prompt
+     * @param user       a user
+     * @param lp         the program's language presenter
+     * @param mp         the program's menu presenter
+     */
+    public VideoStudioActionFactory(UserPrompt userPrompt, User user, LanguagePresenter lp, MenuPresenter mp) {
         this.userPrompt = userPrompt;
         this.user = user;
         this.lp = lp;
         this.mp = mp;
-        this.video = video;
     }
 
+    /**
+     * Generates and returns an action base on the type.
+     *
+     * @param type the type of action
+     * @return an action with the given type
+     */
     public Action getAction(String type) {
         switch (type) {
             case "view videos uploaded":
