142285a0270bc212f2d0579d7772e6dc3cb9ac58
makathe2 <katee.ma@mail.utoronto.ca>
2022-08-10 15:38:42 -0400

    got rid unneeded enums

--- phase2/src/controllers/actions/videoInteractionMenu/EditComment.java
+++ phase2/src/controllers/actions/videoInteractionMenu/EditComment.java
@@ -1,4 +1,5 @@
 package controllers.actions.videoInteractionMenu;
+
 import controllers.actions.MenuAction;
 
 import entities.User;
@@ -10,23 +11,24 @@ import userInterfaces.menuEnums.MenuEnums;
 import userInterfaces.menuFactories.MenuFactory;
 import userInterfaces.menuFactories.UserMenuFactory;
 import userInterfaces.userPrompt.UserPrompt;
+
 public class EditComment extends MenuAction {
     private final Video v;
+
     public EditComment(UserPrompt userPrompt, User user, LanguagePresenter lp, MenuPresenter mp, Video v) {
         currentUser = user;
         this.userPrompt = userPrompt;
         this.lp = lp;
         this.mp = mp;
-        this.v=v;
+        this.v = v;
     }
 
     @Override
     public void run() {
         NonAdminManager NAM = new NonAdminManager(vm);
-//        String uniqueID = userPrompt.getUserStringInput(LanguagePresenter.RequestTextType.EDITCOMMENT);
         String newComm = userPrompt.getUserStringInput(LanguagePresenter.RequestTextType.NEWCOMMENT);
 
-        if (NAM.editComment(v.getUniqueID(), currentUser, newComm)){
+        if (NAM.editComment(v.getUniqueID(), currentUser, newComm)) {
             mp.displayAlert(LanguagePresenter.AlertTextType.EDITCOMMENT);
         } else {
             mp.displayError(LanguagePresenter.ErrorTextType.EDITCOMMENT);
@@ -39,7 +41,7 @@ public class EditComment extends MenuAction {
         if (!um.getRole(currentUser)) {
             MenuFactory userMenuFactory = new UserMenuFactory(userPrompt, currentUser, lp, mp);
             userMenuFactory.getMenu(MenuEnums.NONADMIN).run();
-        }else{
+        } else {
             MenuFactory userMenuFactory = new UserMenuFactory(userPrompt, currentUser, lp, mp);
             userMenuFactory.getMenu(MenuEnums.ADMIN).run();
         }
