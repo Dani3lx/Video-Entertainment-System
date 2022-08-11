0fc9b4f0365cdfe2d69055247d20a3d7b77d3683
sfnicholas <ktspsnicholas@gmail.com>
2022-08-10 20:27:48 -0400

    add javadoc

--- phase2/src/entities/Comments.java
+++ phase2/src/entities/Comments.java
@@ -6,7 +6,13 @@ public class Comments{
     private String comment;
     private String comment_date;
 
-
+    /**
+     * Constructs a comment with commenter, comment and comment_date
+     *
+     * @param commenter the username who make the comment
+     * @param comment the content of the comment
+     * @param comment_date the date and time of the comment made.
+     */
     public Comments(String commenter, String comment, String comment_date){
         this.commenter = commenter;
         this.comment=comment;
@@ -14,26 +20,56 @@ public class Comments{
 
     }
 
+    /**
+     * Returns the commenter of the comment.
+     *
+     * @return the commenter of the comment.
+     */
     public String getCommenter(){
         return commenter;
     }
 
+    /**
+     * Returns the content of the comment.
+     *
+     * @return the content of the comment.
+     */
     public String getComment(){
         return comment;
     }
 
+    /**
+     * Returns the date and time of the comment made.
+     *
+     * @return the date and time of the comment made.
+     */
     public String getComment_date(){
         return comment_date;
     }
 
+    /**
+     * Edit the content of the comment
+     *
+     * @param comment the content of the comment
+     */
     public void setComment(String comment){
         this.comment = comment;
     }
 
+    /**
+     * Edit the date and time of the comment made.
+     *
+     * @param comment_date the date and time of the comment made.
+     */
     public void setComment_date(String comment_date){
         this.comment_date = comment_date;
     }
 
+    /**
+     * Returns the string representation of the comment.
+     *
+     * @return the string representation of the comment
+     */
     @Override
     public String toString(){
         return commenter + ";" + comment + ";" + comment_date;
